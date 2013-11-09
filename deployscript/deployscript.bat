818021d0738affe9cf9d09ec7510fb57eb68f079
vishwanatharondekar <vishwanatharondekar@gmail.com>
2013-11-09 23:40:17 +0530

    Deployscript.

--- deployscript/deployscript.bat
+++ deployscript/deployscript.bat
@@ -15,7 +15,7 @@ cd ..\r-js-optimizer\
 call optimize.cmd
 cd ..\deployscript\
 
-call "C:\Users\vishwanath\Downloads\Compressed\appengine-java-sdk-1.8.3\appengine-java-sdk-1.8.3\bin\appcfg.cmd" update ../war
+call "D:\chrome-downloads\appengine-java-sdk-1.8.6\bin\appcfg.cmd" update ../war
 
 echo "Coming back to local"
 "C:\Program Files (x86)\GnuWin32\bin\sed" -ci s/mode='dev'/mode='local'/g ..\war\static-resources\core\envvariables.js
