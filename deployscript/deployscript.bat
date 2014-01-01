30a0e03b4cca9e4b52335d78bfc7d4b7665cbc86
vishwanatharondekar <vishwanatharondekar@gmail.com>
2014-01-01 13:09:59 +0530

    Removing fem.appcache from build process.

--- deployscript/deployscript.bat
+++ deployscript/deployscript.bat
@@ -1,7 +1,7 @@
 echo "substituting dev settings"
 "C:\Program Files (x86)\GnuWin32\bin\sed" -ci s/mode='local'/mode='dev'/g ..\war\static-resources\core\envvariables.js
 "C:\Program Files (x86)\GnuWin32\bin\sed" -ci s/\/static-resources\//\/built-static-resources\//g ../war/boilerplate.js
-"C:\Program Files (x86)\GnuWin32\bin\sed" -ci s/'fem.appcache'/'builtfem.appcache'/g ../war/index.html
+REM "C:\Program Files (x86)\GnuWin32\bin\sed" -ci s/'fem.appcache'/'builtfem.appcache'/g ../war/index.html
 "C:\Program Files (x86)\GnuWin32\bin\sed" -ci s/\"static-resources\//\"built-static-resources\//g ../war/index.html
 "C:\Program Files (x86)\GnuWin32\bin\sed" -ci s/require.js/require.min.js/g ../war/index.html
 "C:\Program Files (x86)\GnuWin32\bin\sed" -ci s/1\.0/1\.0\.1/g ../war/builtfem.appcache
@@ -12,15 +12,17 @@ echo "substituting dev settings"
 
 
 cd ..\r-js-optimizer\
-call optimize.cmd
+REM call optimize.cmd
 cd ..\deployscript\
+
+break 
 
 call "D:\chrome-downloads\appengine-java-sdk-1.8.6\bin\appcfg.cmd" update ../war
 
 echo "Coming back to local"
 "C:\Program Files (x86)\GnuWin32\bin\sed" -ci s/mode='dev'/mode='local'/g ..\war\static-resources\core\envvariables.js
 "C:\Program Files (x86)\GnuWin32\bin\sed" -ci s/built-static-resources/static-resources/g ../war/boilerplate.js
-"C:\Program Files (x86)\GnuWin32\bin\sed" -ci s/'builtfem.appcache'/'fem.appcache'/g ../war/index.html
+REM "C:\Program Files (x86)\GnuWin32\bin\sed" -ci s/'builtfem.appcache'/'fem.appcache'/g ../war/index.html
 "C:\Program Files (x86)\GnuWin32\bin\sed" -ci s/\"built-static-resources\//\"static-resources\//g ../war/index.html
 "C:\Program Files (x86)\GnuWin32\bin\sed" -ci s/1\.0\.1\.1\.1\.1\.1/1\.0/g ../war/builtfem.appcache
 "C:\Program Files (x86)\GnuWin32\bin\sed" -ci s/require.min.js/require.js/g ../war/index.html
