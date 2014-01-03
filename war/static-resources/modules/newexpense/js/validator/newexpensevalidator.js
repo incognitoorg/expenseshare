382947cd28c05fc125de5e9133585403a7ccf6a0
vishwanatharondekar <vishwanatharondekar@gmail.com>
2014-01-03 23:15:59 +0530

    Registering rules with respect to context.

--- war/static-resources/modules/newexpense/js/validator/newexpensevalidator.js
+++ war/static-resources/modules/newexpense/js/validator/newexpensevalidator.js
@@ -15,12 +15,12 @@ define( function(require){
 			options.rules = this.rules;//TODO Need to find better way of this
 			options.messages = this.messages;//TODO Need to find better way of this
 			FormValidator.registertestValidator(options);
-			$("[name^=expenseInput]").each(function () {
+			options.element.find("[name^=expenseInput]").each(function () {
 				$(this).rules("add", {
 					expenseInputValidator: true
 				});
 			});
-			$(".js-pay-input").each(function () {
+			options.element.find(".js-pay-input").each(function () {
 				$(this).rules("add", {
 					"require_from_group" : [1, '.js-pay-input']
 				});
