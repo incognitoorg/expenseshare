c733e684a575b44adf48f63d03ac91c7092dd743
vishwanatharondekar <vishwanatharondekar@gmail.com>
2014-01-12 19:48:05 +0530

    Using node. Requires. 1. Node, install for your env 2. requirejs as node module - on command prompt    npm install -g requirejs

--- r-js-optimizer/optimize.sh
+++ r-js-optimizer/optimize.sh
@@ -1,1 +1,2 @@
-java -classpath lib/rhino/js.jar:lib/closure/compiler.jar org.mozilla.javascript.tools.shell.Main tools/r.js -o tools/build.js
+REM java -classpath lib\rhino\js.jar;lib\closure\compiler.jar org.mozilla.javascript.tools.shell.Main tools\r.js -o tools\build.js
+node tools\r.js -o tools\build.js
