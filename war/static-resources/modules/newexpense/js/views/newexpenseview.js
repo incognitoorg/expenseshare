c16c9b4eea5eedd13696bdbdca218f3ed3bdff88
vishwanatharondekar <vishwanatharondekar@gmail.com>
2014-02-09 18:48:54 +0530

    Selected friend css.

--- war/static-resources/modules/newexpense/js/views/newexpenseview.js
+++ war/static-resources/modules/newexpense/js/views/newexpenseview.js
@@ -270,7 +270,7 @@ define(function(require) {
 				var friendInfo		= self.selectedFriends[i];
 				var fullname  = friendInfo.fullName;
 				var imgUrl    = friendInfo.imgURL;
-				htmlContain+= '<div class="small-12 large-6 columns">'+'<img src="'+imgUrl+'" style="padding:0 0 10px 5px;"></img><span style="padding-left:10px;">' + fullname +'</span></div>';
+				htmlContain+= '<div class="small-12 large-6 columns selected-friend">'+'<img src="'+imgUrl+'" style="padding:0 0 10px 5px;"></img><span style="padding-left:10px;">' + fullname +'</span></div>';
 			}	
 			$('.js-friend-selector').html(htmlContain);	
 		},
