d4a23b3b5dbd0069fc18739d77c4c49522bd5724
varonde <vishwanath.arondekar@morningstar.com>
2014-02-10 12:03:56 +0530

    Tasks created for upload.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,20 +2,11 @@ module.exports = function(grunt) {
 
 	var requirejsconfig = grunt.file.readJSON('./r-js-optimizer/tools/build.js');
 	/*console.log('requirejsconfig', requirejsconfig)*/
-	
+
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		uglify: {
-			options: {
-				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-			},
-			build: {
-				src: 'src/<%= pkg.name %>.js',
-				dest: 'build/<%= pkg.name %>.min.js'
-			}
-		},
-		replace: {
+		/*replace: {
 			production: {
 				env : {
 					src: ['war/static-resources/core/envvariables.js'],             // source files array (supports minimatch)
@@ -29,6 +20,24 @@ module.exports = function(grunt) {
 							return '/built-static-resources/';
 						}
 					}]
+					files: {
+						'path/to/directory/': 'path/to/source/*', // includes files in dir
+					      'path/to/directory/': 'path/to/source/**', // includes files in dir and subdirs
+					      'path/to/project-<%= pkg.version %>/': 'path/to/source/**', // variables in destination
+					      'path/to/directory/': ['path/to/sources/*.js', 'path/to/more/*.js'], // include JS files in two diff dirs
+						 			      'war/static-resources/core/envvariables.js': 'war/static-resources/core/envvariables.js'
+					},
+					options: {
+						replacements: [{ 
+							from: "mode='local'",                   // string replacement
+							to: "mode='dev'" 
+						}, {
+								from: 'Foo',
+								to: function (matchedWord) {   // callback replacement
+									return matchedWord + ' Bar';
+								}
+							}]
+					}
 				}
 			},
 			local: {
@@ -37,14 +46,14 @@ module.exports = function(grunt) {
 				replacements: [{ 
 					from: "mode='dev'",                   // string replacement
 					to: "mode='local'" 
-				}/*, {
+				}, {
 					from: 'Foo',
 					to: function (matchedWord) {   // callback replacement
 						return matchedWord + ' Bar';
 					}
-				}*/]
+				}]
 			}
-		},
+		},*/
 		appengine: {
 			options: {
 				sdk:'D:/chrome-downloads/appengine-java-sdk-1.8.6/bin',
@@ -71,60 +80,58 @@ module.exports = function(grunt) {
 				options : requirejsconfig 
 			} 
 		},
-		
+
 		/*(function(){
 			console.log(JSON.parse(grunt.file.read("r-js-optimizer/tools/build.js"));
 			return JSON.parse(grunt.file.read("r-js-optimizer/tools/build.js");
 		})()*/
-		
+
 		'string-replace': {
-			  dist: {
-			    files: {
-			      'path/to/directory/': 'path/to/source/*', // includes files in dir
-			      'path/to/directory/': 'path/to/source/**', // includes files in dir and subdirs
-			      'path/to/project-<%= pkg.version %>/': 'path/to/source/**', // variables in destination
-			      'path/to/directory/': ['path/to/sources/*.js', 'path/to/more/*.js'], // include JS files in two diff dirs
-			      'path/to/filename.ext': 'path/to/source.ext'
-			    },
-			    options: {
-			      replacements: [{
-			        pattern: /\/(asdf|qwer)\//ig,
-			        replacement: '$1'
-			      }, {
-			        pattern: ',',
-			        replacement: ';'
-			      }]
-			    }
-			  },
-			  inline: {
-			    options: {
-			      replacements: [
-			        // place files inline example
-			        {
-			            pattern: '<script></script>',
-			            replacement: '<script></script>'
-			          }
-			      ]
-			    }
-			  }
+			dist: {
+				files: {
+					/*'path/to/directory/': 'path/to/source/*', // includes files in dir
+				      'path/to/directory/': 'path/to/source/**', // includes files in dir and subdirs
+				      'path/to/project-<%= pkg.version %>/': 'path/to/source/**', // variables in destination
+				      'path/to/directory/': ['path/to/sources/*.js', 'path/to/more/*.js'], // include JS files in two diff dirs
+					 */			      
+					'war/static-resources/core/envvariables.js': 'war/static-resources/core/envvariables.js',
+					'war/boilerplate.js': 'war/boilerplate.js',
+					'war/index.html': 'war/index.html',
+				},
+				options: {
+					replacements: [{
+						pattern: "/static-resources/",
+						replacement: "/built-static-resources/"
+					}, {
+						pattern: "mode='local'",
+						replacement: "mode='dev'"
+					},
+					 {
+						pattern: "'fem.appcache'",
+						replacement: "'builtfem.appcache'"
+					},
+					 {
+						pattern: "require.js",
+						replacement: "require.min.js"
+					}]
+				}
 			}
+		}
 
 	});
 
 	// Load the plugin that provides the "uglify" task.
-	//grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-text-replace');
 	grunt.loadNpmTasks('grunt-string-replace');
 	grunt.loadNpmTasks('grunt-appengine');
 	grunt.loadNpmTasks('grunt-contrib-requirejs');
-	
-	
-	console.log('printing gruntconfig', grunt.config().requirejs);
+
 
 	// Default task(s).
 	grunt.registerTask('default', ['replace']);
 
 };
+
 
 
 //echo "substituting dev settings"
