6ba6f679750a7ab00668f44bf5285bec2393da68
vishwanatharondekar <vishwanatharondekar@gmail.com>
2014-03-28 18:21:19 +0530

    Fixed bug of new string.

--- src/com/fem/util/MailUtil.java
+++ src/com/fem/util/MailUtil.java
@@ -27,6 +27,45 @@ public class MailUtil {
 	}
 	
 	
+	
+	public void sendToOne(String subject, String msgContent, String emailId){
+		log.info("In sendToOne() of MailUtil...."); 
+		
+		StringBuilder sbMsgContent = new StringBuilder(msgContent);
+
+		try {
+			Properties props = new Properties();
+			Session session = Session.getDefaultInstance(props, null);
+
+			MimeMessage msg = new MimeMessage(session);
+			String SENDER_EMAIL_ADDRESS = PropertiesUtil.getProperty("SENDER_EMAIL_ADDRESS");
+			String SENDER_NAME = PropertiesUtil.getProperty("SENDER_NAME");
+			msg.setFrom(new InternetAddress(SENDER_EMAIL_ADDRESS, SENDER_NAME));
+
+			msg.addRecipient(Message.RecipientType.TO, new InternetAddress(emailId));
+			log.info("User email added - " + emailId);
+			
+			System.out.println(sbMsgContent);
+
+			msg.setSubject(subject);
+			msg.setText(sbMsgContent.toString(), "utf-8", "html");
+			Transport.send(msg);
+
+			log.info("Mail sent successfully to user in sendToOne");
+			
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			log.log(Level.SEVERE, e.getStackTrace().toString());
+		} catch (MessagingException e) {
+			e.printStackTrace();
+			log.log(Level.SEVERE, e.getStackTrace().toString());
+		} catch (Exception e){
+			e.printStackTrace();
+			log.log(Level.SEVERE, e.getStackTrace().toString());
+		}
+	}
+	
+	
 	public void sendToAll(String subject, String msgContent, HashMap<String, String> hmEmailIds) {
 
 		log.info("In sendMail() of MailUtil...."); 
@@ -42,43 +81,37 @@ public class MailUtil {
 			String SENDER_NAME = PropertiesUtil.getProperty("SENDER_NAME");
 			msg.setFrom(new InternetAddress(SENDER_EMAIL_ADDRESS, SENDER_NAME));
 
-			if(hmEmailIds == null) {
-				msg.addRecipient(Message.RecipientType.TO, new InternetAddress("admins"));
-				log.info("Admin only mails " + PropertiesUtil.getProperty("ADMINS"));
+			for (Map.Entry<String, String> entry : hmEmailIds.entrySet()) { 
+				msg.addRecipient(Message.RecipientType.TO, new InternetAddress(entry.getKey(), entry.getValue()!=null ? entry.getValue() : "User"));
+				log.info("User email added - " + entry.getKey());
 				
-				System.out.println(sbMsgContent);
+				int index = sbMsgContent.indexOf("??username??");
+				
+				String emailContent = new String(sbMsgContent);
+				StringBuilder sbEmailContent = new StringBuilder(emailContent);
+				
+				sbEmailContent.replace(index, index + 12, entry.getValue()!=null ? entry.getValue() : "User");
+				
+				//TODO : Sending email to admin every time it is sent to user. This should be removed going ahead as app becomes big.
+				msg.addRecipient(Message.RecipientType.BCC, new InternetAddress("admins"));
+				log.info("Admin added " + PropertiesUtil.getProperty("ADMINS"));
+				
+				System.out.println(sbEmailContent);
 
 				msg.setSubject(subject);
 				msg.setText(sbMsgContent.toString(), "utf-8", "html");
 				Transport.send(msg);
 
 				log.info("Mail sent successfully");
-			} else {
-				for (Map.Entry<String, String> entry : hmEmailIds.entrySet()) { 
-					msg.addRecipient(Message.RecipientType.TO, new InternetAddress(entry.getKey(), entry.getValue()!=null ? entry.getValue() : "User"));
-					log.info("User email added - " + entry.getKey());
-					
-					int index = sbMsgContent.indexOf("??username??");
-					sbMsgContent.replace(index, index + 12, entry.getValue()!=null ? entry.getValue() : "User");
-					
-					//msg.addRecipient(Message.RecipientType.BCC , new InternetAddress(PropertiesUtil.getProperty("ADMINS")));
-					msg.addRecipient(Message.RecipientType.TO, new InternetAddress("admins"));
-					log.info("Admin added " + PropertiesUtil.getProperty("ADMINS"));
-					
-					System.out.println(sbMsgContent);
-
-					msg.setSubject(subject);
-					msg.setText(sbMsgContent.toString(), "utf-8", "html");
-					Transport.send(msg);
-
-					log.info("Mail sent successfully");
-				}
 			}
 			
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 			log.log(Level.SEVERE, e.getStackTrace().toString());
 		} catch (MessagingException e) {
+			e.printStackTrace();
+			log.log(Level.SEVERE, e.getStackTrace().toString());
+		} catch (Exception e){
 			e.printStackTrace();
 			log.log(Level.SEVERE, e.getStackTrace().toString());
 		}
