f1be861947cb8cbf48714504b09711991dca6916
Srikanth Subramanian <srikanth.manian@gmail.com>
2014-03-30 19:50:41 +0530

    Created the UI and necessary events for implementing Register, Email Login & Forgot Password. Created separate page for set password flow.

--- war/static-resources/components/login/js/views/loginview.js
+++ war/static-resources/components/login/js/views/loginview.js
@@ -11,6 +11,17 @@ define(function(require){
 			google : GoogleAPI
 	};
 	
+	var formContainers = {
+			"show-signin": "app-signin",
+			"show-signup": "app-signup",
+			"show-forgot-password": "app-forgot-password"
+	};
+
+	function validateEmail(email) {
+		var regEx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	    return regEx.test(email);
+	}
+
 	var LoginView = Backbone.View.extend({
 		initialize : function(options){
 			$('#mask').html('Checking if logged in....');
@@ -30,12 +41,19 @@ define(function(require){
 			hideMask();
 			initializationComplete = true;
 		},
+		template: Handlebars.compile(require("text!./../../templates/loginform.html")),
 		render : function(){
-			//$(this.el).html(this.template());
+			$(this.el).html(this.template());
 		},
 		events : {
 			'click .facebook' : 'eventDoFacebookLogin',
-			'click .google' : 'eventDoGoogleLogin'
+			'click .google' : 'eventDoGoogleLogin',
+			'click .email-signin' : 'eventDoEmailLogin',
+			'click .email-signup' : 'eventDoEmailSignup',
+			'click .retrieve-password' : 'eventDoForgotPassword',
+			'click .show-signup' : 'eventShowForm',
+			'click .show-forgot-password' : 'eventShowForm',
+			'click .show-signin' : 'eventShowForm'
 			
 		},
 		eventDoFacebookLogin : function(options){
@@ -66,6 +84,60 @@ define(function(require){
 					this.doActualLogin.call(this, data);
 				}
 			}, context : this});
+		},
+		getUserAccessInfo : function (userControl, passControl) {
+			/*using the form controls provided to get the values*/
+			var useremail = this.$("#" + userControl).val(),
+				/*creating the object to send to the back-end*/
+				userControl = {};
+			if(passControl) {
+				var userpass = this.$("#" + passControl).val();
+			}
+
+			if (useremail !== "" && validateEmail(useremail)) {
+				userControl["email"] = useremail;
+				if (userpass) {
+					/*adding password identifier only if password form control has been sent*/
+					userControl["password"] = userpass;
+				}
+			}
+			return userControl;
+		},
+		eventDoEmailLogin: function (event) {
+			if (this.$("#app-access-control")[0].checkValidity()) {
+				/*preventing form submit event*/
+				event.preventDefault();
+			}
+			/*getting the form controls and obtaining values to send data*/
+			var userInfo = this.getUserAccessInfo("username", "password");
+			console.log("userInfo", userInfo);
+		},
+		eventDoEmailSignup: function (event) {
+			if (this.$("#app-access-control")[0].checkValidity()) {
+				event.preventDefault();
+			}
+			/*getting the form controls and obtaining values to send data*/
+			var userInfo = this.getUserAccessInfo("signup-username");
+			console.log("userInfo", userInfo);
+		},
+		eventDoForgotPassword: function (event) {
+			if (this.$("#app-access-control")[0].checkValidity()) {
+				event.preventDefault();
+			}
+			/*getting the form controls and obtaining values to send data*/
+			var userInfo = this.getUserAccessInfo("forgot-pass-username");
+			console.log("userInfo", userInfo);
+		},
+		eventShowForm: function (event) {
+			var currentTarget = event.currentTarget.className;
+			/*removing the form validation controls, since while submitting form, hidden controls are also validated.*/
+			this.$(".form-container").find(".required-inputs").prop("autofocus", false);
+			this.$(".form-container").find(".required-inputs").prop("required", false);
+			this.$(".form-container").hide();
+			this.$("#" + formContainers[currentTarget]).show();
+			/*applying form validation controls to only the currently visible form elements*/
+			this.$("#" + formContainers[currentTarget]).find(".required-inputs").prop("autofocus", true);
+			this.$("#" + formContainers[currentTarget]).find(".required-inputs").prop("required", true);
 		},
 		doActualLogin : function(data){
 			showMask('Getting your information...');
