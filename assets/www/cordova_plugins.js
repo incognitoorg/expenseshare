3aa7ced810e47eadb103c4f882e83a5a8e8c31ed
vishwanatharondekar <vishwanatharondekar@gmail.com>
2014-04-20 20:41:05 +0530

    Upgraded to cordova 3.4.0

--- assets/www/cordova_plugins.js
+++ assets/www/cordova_plugins.js
@@ -6,6 +6,28 @@ module.exports = [
         "clobbers": [
             "window.open"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/network.js",
+        "id": "org.apache.cordova.network-information.network",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/Connection.js",
+        "id": "org.apache.cordova.network-information.Connection",
+        "clobbers": [
+            "Connection"
+        ]
     }
 ]
+
+module.exports.metadata = 
+	// TOP OF METADATA
+	{
+	    "org.apache.cordova.network-information": "0.2.7"
+	}
+
 });
