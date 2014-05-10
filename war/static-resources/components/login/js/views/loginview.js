18afd5dfba2a95b1e95a9efc1a445d153fae9aff
vishwanatharondekar <vishwanatharondekar@gmail.com>
2014-05-10 14:37:24 +0530

    Register and Set password interstetial page and success message.

--- war/static-resources/components/login/js/views/loginview.js
+++ war/static-resources/components/login/js/views/loginview.js
@@ -6,6 +6,8 @@ define(function(require){
 	
 	var GoogleAPI = require('googleapioauth');
 	var FBAPI = require('fbapioauth');
+	var registerSuccessTemplate = Handlebars.compile(require('text!../../templates/register-success.html'));
+	var passwordResetSuccessTemplate = Handlebars.compile(require('text!../../templates/password-reset-success.html'));
 	
 	var APIMapper = {
 			facebook : FBAPI,
@@ -105,7 +107,7 @@ define(function(require){
 			
 			if(userInfo.nameControl) {
 				var userFullName = this.$("#" + userInfo.nameControl).val();
-				userControl["fullName"] = userFullName;
+				userFullName && (userControl["fullName"] = userFullName);
 			}
 
 			if (useremail !== "" && validateEmail(useremail)) {
@@ -145,11 +147,21 @@ define(function(require){
 						"nameControl": "signup-name"
 				};
 				var userInfo = this.getUserAccessInfo(userInfoElements);
+				showMask('Registering you with us...');
 				var ajaxOptions = {
 						url : '_ah/api/userendpoint/v1/user/register',
 						callback : function(response){
-							var message = "You have been successfully registered with us. A mail has been sent to the account specified by you for verification, wherein you can activate your account by clicking the link provided in the mail.",
+							//var message = "You have been successfully registered with us. A mail has been sent to the account specified by you for verification, wherein you can activate your account by clicking the link provided in the mail.",
+							var registerTemplateData = {
+									emailDomain : userInfo.email.substr(userInfo.email.indexOf('@')+1)
+							};
+							
+							_.extend(registerTemplateData, userInfo);
+							var message = registerSuccessTemplate(registerTemplateData);
+							
 							state = "success";
+							self.$('#app-access-control').hide();
+							hideMask();
 							showHideToasters(this, message, state);
 						}, 
 						errorCallback : this.somethingBadHappend,
@@ -166,14 +178,42 @@ define(function(require){
 		},
 		eventDoForgotPassword: function (event) {
 			if (this.$("#app-access-control")[0].checkValidity()) {
+				
+				/*getting the form controls and obtaining values to send data*/
+				var userInfoElements = {
+						"userControl": "forgot-pass-username"
+				};
+				var userInfo = this.getUserAccessInfo(userInfoElements);
+				showMask('Verifying user and setting password...');
+				console.log("userInfo", userInfo);
+				
+				var ajaxOptions = {
+						url : '_ah/api/userendpoint/v1/user/register',
+						callback : function(response){
+							var registerTemplateData = {
+									emailDomain : userInfo.email.substr(userInfo.email.indexOf('@')+1)
+							};
+							
+							_.extend(registerTemplateData, userInfo);
+							var message = passwordResetSuccessTemplate(registerTemplateData);
+							
+							state = "success";
+							self.$('#app-access-control').hide();
+							hideMask();
+							showHideToasters(this, message, state);
+						}, 
+						errorCallback : this.somethingBadHappend,
+						context : this,
+						dataType: 'json',
+						contentType: 'application/json',
+						type : 'POST',
+						data : userInfo
+				}
+				Sandbox.doPost(ajaxOptions);
+				
 				event.preventDefault();
 			}
-			/*getting the form controls and obtaining values to send data*/
-			var userInfoElements = {
-					"userControl": "forgot-pass-username"
-			};
-			var userInfo = this.getUserAccessInfo(userInfoElements);
-			console.log("userInfo", userInfo);
+			
 		},
 		eventShowForm: function (event) {
 			var currentTarget = event.currentTarget.className;
